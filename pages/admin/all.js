400576a4472c9c56b8d97b778ca90b77da2bf701
UJustBe Tech <88655561+ujustbetech@users.noreply.github.com>
2023-02-26 16:01:24 +0530

    upload on prod

--- pages/admin/all.js
+++ pages/admin/all.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
 function all() {
-
+  // test
     const [alldata, setAllData] = useState([])
 
     useEffect(() => {
